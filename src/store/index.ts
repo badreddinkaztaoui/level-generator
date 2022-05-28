507aa9ac7a51b6745d47dce0ae9d12d52b985a43
Badr Eddin Kaztaoui <kaztaouibadreddin@gmail.com>
2022-05-28 06:31:58 +0100

    Fix dependencies issue

--- src/store/index.ts
+++
@@ -1,7 +0,0 @@
-// Global State
-import { createGlobalState } from "react-hooks-global-state";
-
-const initialState = { score: 0 };
-const { setGlobalState, useGlobalState } = createGlobalState(initialState);
-
-export { setGlobalState, useGlobalState };
